603b51f458bb54c23301ca4f361f0f49e6fc086c
Nimish Bhandari <nimishbhandari727521@gmail.com>
2022-03-07 23:40:47 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>FrontPage</title>
         <meta name="keyword" content="web development" />
       </Head>
-      <h1>Nextapp</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+
+  const articles = await res.json();
+  return {
+    props: {
+      articles,
+    },
+  };
+};
